3ca58d26e2e6f93b826b7c6e88ee782fe00e83e7
Mark-R-5388 <markrose@markwebdev.com>
2022-07-06 12:37:34 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import random
+
+print("...rock...")
+print("...paper...")
+print("...scissors...")
+player1 = input("enter player 1's choice: ").strip().lower()
+
+
+computer = random.randint(1,3)
+
+if computer == 1:
+  computer = "rock"
+elif computer == 2:
+  computer= "scissors"
+else:
+  computer = "paper"
+
+print(f"computer chose {computer}")
+if player1 == computer:
+	print("Its a tie!")
+
+elif player1 == "rock":
+	if computer == "scissors":
+		print("rock beats scissors, Player 1 wins")
+	elif computer == "paper":
+		print("paper covers rock, player 2 wins")
+
+elif player1 == "scissors":
+	if computer == "paper":
+		print("scissors cuts paper, player 1 wins")
+	if computer == "rock":
+		print("rock smashes scissors, player 2 wins")
+
+elif player1 == "paper":
+	if computer == "rock":
+		print("paper covers rock, player 1 wins")
+	if computer == "scissors":
+		print("scissors cuts paper, player 2 wins")
+
+else:
+	print("Something went wrong")
